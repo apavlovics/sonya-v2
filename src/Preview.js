198d8339b653ac145d18171df71b224ce9a7241d
Andrejs Pavlovics <andrey.pavlovich@me.com>
2020-10-29 23:48:39 +0200

    Migrate to React

--- src/Preview.js
+++ src/Preview.js
@@ -2,30 +2,19 @@ import React from 'react'
 
 class Preview extends React.Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      size: props.size,
-      url: props.url,
-      title: props.title,
-      year: props.year,
-      focused: props.focused,
-      desaturated: props.desaturated,
-    }
-  }
-
   render() {
-    const extraClassName = this.state.focused ? " focused" : this.state.desaturated ? " desaturated" : ""
+    const extraClassName = this.props.focused ? " focused" : this.props.desaturated ? " desaturated" : ""
     return (
       <div
-        className={`preview ${this.state.size}${extraClassName}`}
-        style={{backgroundImage: `url(/projects/${this.state.url}/cover.jpg)`}}
-        onMouseEnter={() => this.setState({focused: true})}
-        onMouseLeave={() => this.setState({focused: false})}>
-        <a href={`/design/${this.state.url}/`}>
-          <div className={`details${this.state.focused ? " visible" : ""}`}>
-            <h2>{this.state.title}</h2>
-            <span>{this.state.year}</span>
+        className={`preview ${this.props.size}${extraClassName}`}
+        style={{backgroundImage: `url(/projects/${this.props.url}/cover.jpg)`}}
+        onMouseEnter={() => this.props.onMouseEnter()}
+        onMouseLeave={() => this.props.onMouseLeave()}
+      >
+        <a href={`/design/${this.props.url}/`}>
+          <div className={`details${this.props.focused ? " visible" : ""}`}>
+            <h2>{this.props.title}</h2>
+            <span>{this.props.year}</span>
           </div>
         </a>
       </div>
--- src/Previews.js
+++ src/Previews.js
@@ -11,41 +11,68 @@ class Previews extends React.Component {
         url: "jauna-teika",
         title: "Jauna Teika Apartment",
         year: "2020",
+        focused: false,
+        desaturated: false,
       }, {
         size: "medium",
         url: "andrey-silchenko",
         title: "Andrey Silchenko Hair Salon",
         year: "2016",
+        focused: false,
+        desaturated: false,
       }, {
         size: "medium",
         url: "open-space",
         title: "Open Space in Central Riga",
         year: "2015",
+        focused: false,
+        desaturated: false,
       }, {
         size: "large",
         url: "round-house",
         title: "Round House Apartment",
         year: "2012",
+        focused: false,
+        desaturated: false,
       }]
     }
   }
 
-  renderPreview(preview) {
-    return (
-      <Preview
-        key={preview.url}
-        size={preview.size}
-        url={preview.url}
-        title={preview.title}
-        year={preview.year}
-      />
-    )
+  togglePreviews(currentPreview, focused) {
+    const previews = this.state.previews.map(p => {
+      if (focused) {
+        if (p == currentPreview) {
+          p.focused = focused
+          p.desaturated = !focused
+        } else {
+          p.focused = !focused
+          p.desaturated = focused
+        }
+      } else {
+          p.focused = false
+          p.desaturated = false
+      }
+      return p
+    })
+    this.setState({previews: previews})
   }
 
   render() {
     return (
       <main>
-        {this.state.previews.map(this.renderPreview)}
+        {this.state.previews.map(preview => (
+          <Preview
+            key={preview.url}
+            size={preview.size}
+            url={preview.url}
+            title={preview.title}
+            year={preview.year}
+            focused={preview.focused}
+            desaturated={preview.desaturated}
+            onMouseEnter={() => this.togglePreviews(preview, true)}
+            onMouseLeave={() => this.togglePreviews(preview, false)}
+          />
+        ))}
       </main>
     )
   }
