149b7af671874b76f2e2ee693586c2f41c38c18b
Andrejs Pavlovics <andrey.pavlovich@me.com>
2020-10-30 12:18:09 +0200

    Optimize JavaScript

--- src/Preview.js
+++ src/Preview.js
@@ -1,24 +1,19 @@
-import React from 'react'
-
-class Preview extends React.Component {
-
-  render() {
-    const extraClassName = this.props.focused ? " focused" : this.props.desaturated ? " desaturated" : ""
-    return (
-      <div
-          className={`preview ${this.props.size}${extraClassName}`}
-          style={{backgroundImage: `url(/projects/${this.props.url}/cover.jpg)`}}
-          onMouseEnter={() => this.props.onMouseEnter()}
-          onMouseLeave={() => this.props.onMouseLeave()}>
-        <a href={`/design/${this.props.url}/`}>
-          <div className={`details${this.props.focused ? " visible" : ""}`}>
-            <h2>{this.props.title}</h2>
-            <span>{this.props.year}</span>
-          </div>
-        </a>
-      </div>
-    )
-  }
+const Preview = props => {
+  const extraClassName = props.focused ? " focused" : props.desaturated ? " desaturated" : ""
+  return (
+    <div
+        className={`preview ${props.size}${extraClassName}`}
+        style={{backgroundImage: `url(/projects/${props.url}/cover.jpg)`}}
+        onMouseEnter={() => props.onMouseEnter()}
+        onMouseLeave={() => props.onMouseLeave()}>
+      <a href={`/design/${props.url}/`}>
+        <div className={`details${props.focused ? " visible" : ""}`}>
+          <h2>{props.title}</h2>
+          <span>{props.year}</span>
+        </div>
+      </a>
+    </div>
+  )
 }
 
 export default Preview
