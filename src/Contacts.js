758ff7b874e76f41d861113d51e6ad579af31dfd
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2020-11-10 22:41:06 +0200

    Add .contacts class

--- src/Contacts.js
+++ src/Contacts.js
@@ -6,6 +6,10 @@ export default function Contacts() {
     <main>
       <h1>{t('Contacts')}</h1>
       <p>{t('Contacts Text')}</p>
+      <div className="contacts">
+        +371 2238 0067<br />
+        <a href="mailto:info@smartcasual.lv">info@smartcasual.lv</a>
+      </div>
     </main>
   )
 }
