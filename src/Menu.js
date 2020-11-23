2a11edecb553db324137cb9c239b3f0c505670a2
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2020-11-23 15:36:58 +0200

    Optimize SVGs

--- src/Menu.js
+++ src/Menu.js
@@ -3,8 +3,6 @@ import {Link, withRouter} from 'react-router-dom'
 import {useTranslation} from 'react-i18next'
 import useScrollPosition from '@react-hook/window-scroll'
 import {useWindowWidth} from '@react-hook/window-size'
-import MenuOpen from './icons/menu-open.svg'
-import MenuClose from './icons/menu-close.svg'
 
 function Menu(props) {
   const [t, i18n] = useTranslation()
@@ -31,11 +29,7 @@ function Menu(props) {
       <div>
         <div className="logo">
           <div>Smart Casual</div>
-          <img
-              id={maximized ? 'menu-close' : 'menu-open'} 
-              src={maximized ? MenuClose : MenuOpen}
-              alt={maximized ? 'Close main menu' : 'Open main menu'}
-              onClick={() => setMaximized(!maximized)} />
+          <MenuIcon maximized={maximized} onClick={() => setMaximized(!maximized)} />
         </div>
         <ul className={`main-menu ${stateClassName}`}>
           {props.sections.map(section => (
@@ -81,6 +75,17 @@ const stripSlashes = (path) => {
 
 const stripPrefix = (path, prefix) => {
   return path.startsWith(prefix) ? path.substr(prefix.length) : path
+}
+
+function MenuIcon(props) {
+  return (
+    <svg width="2em" height="2em" onClick={props.onClick} viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+      {props.maximized
+        ? <path fillRule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+        : <path fillRule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+      }
+    </svg>
+  )
 }
 
 function MainMenuItem(props) {
