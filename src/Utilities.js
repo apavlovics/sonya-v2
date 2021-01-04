d8095241a18e2bccd676affd314072b44d2809e1
Andrejs Pavlovics <andrey.pavlovich@me.com>
2021-01-04 22:50:46 +0200

    Improve navigation

--- src/Utilities.js
+++ src/Utilities.js
@@ -1,5 +1,5 @@
 /** Strips one "/" slash from the beginning and the end of the path, if it is present. */
-export const stripSlashes = (path) => {
+export const stripSlashes = path => {
   if (path.charAt(0) === '/') {
     path = path.substr(1)
   }
@@ -12,4 +12,10 @@ export const stripSlashes = (path) => {
 /** Strips the prefix string from the path, if it is present. */
 export const stripPrefix = (path, prefix) => {
   return path.startsWith(prefix) ? path.substr(prefix.length) : path
+}
+
+/** Resolves the parent path by trimming the path to its last slash, if it is present. */
+export const resolveParentPath = path => {
+  const lastSlashIndex = path.lastIndexOf('/')
+  return lastSlashIndex === -1 ? path : path.substr(0, lastSlashIndex + 1)
 }
