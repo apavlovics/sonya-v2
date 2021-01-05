0f671b258453bf960f953bbe71aa3ff519afe47f
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2021-01-05 16:42:06 +0200

    Improve CSS

--- src/Gallery.js
+++ src/Gallery.js
@@ -31,7 +31,7 @@ function Gallery(props) {
             naturalSlideWidth={1}
             isIntrinsicHeight="true"
             totalSlides={3}>
-          <ButtonBack>
+          <ButtonBack className="back-button">
             <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" >
               <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
             </svg>
@@ -41,7 +41,7 @@ function Gallery(props) {
             <Slide index={1}><Image src="/projects/jauna-teika/gallery/002.jpg" /></Slide>
             <Slide index={2}><Image src="/projects/jauna-teika/gallery/003.jpg" /></Slide>
           </Slider>
-          <ButtonNext>
+          <ButtonNext className="next-button">
             <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
             </svg>
