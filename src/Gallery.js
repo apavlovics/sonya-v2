120c58238e73c66e1fd6dffd1abc6ebd3707e89a
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2021-01-06 20:46:43 +0200

    Improve CSS

--- src/Gallery.js
+++ src/Gallery.js
@@ -24,10 +24,10 @@ function Gallery(props) {
           <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
         </svg>
       </button>
-      {/* TODO Add lazy loading of images */}
       <CarouselProvider
           naturalSlideWidth={1}
-          isIntrinsicHeight="true"
+          isIntrinsicHeight
+          hasMasterSpinner
           totalSlides={props.preview.galleryLength}>
         <ButtonBack className="back-button">
           <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" >
