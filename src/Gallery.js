d6d33ec54b3d562fdb44d8b2430e2daafb99aa75
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2021-01-06 17:32:57 +0200

    Improve Gallery layout

--- src/Gallery.js
+++ src/Gallery.js
@@ -24,31 +24,29 @@ function Gallery(props) {
           <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
         </svg>
       </button>
-      <div className="gallery">
-        {/* TODO Add lazy loading of images */}
-        <CarouselProvider
-            naturalSlideWidth={1}
-            isIntrinsicHeight="true"
-            totalSlides={props.preview.galleryLength}>
-          <ButtonBack className="back-button">
-            <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" >
-              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-            </svg>
-          </ButtonBack>
-          <Slider>
-            {[...Array(props.preview.galleryLength).keys()].map(key => (
-              <Slide index={key}>
-                <Image src={`/projects/${props.preview.url}/gallery/00${key + 1}.jpg`} />
-              </Slide>
-            ))}
-          </Slider>
-          <ButtonNext className="next-button">
-            <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-            </svg>
-          </ButtonNext>
-        </CarouselProvider>
-      </div>
+      {/* TODO Add lazy loading of images */}
+      <CarouselProvider
+          naturalSlideWidth={1}
+          isIntrinsicHeight="true"
+          totalSlides={props.preview.galleryLength}>
+        <ButtonBack className="back-button">
+          <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" >
+            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+          </svg>
+        </ButtonBack>
+        <Slider className="gallery">
+          {[...Array(props.preview.galleryLength).keys()].map(key => (
+            <Slide index={key}>
+              <Image src={`/projects/${props.preview.url}/gallery/00${key + 1}.jpg`} />
+            </Slide>
+          ))}
+        </Slider>
+        <ButtonNext className="next-button">
+          <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+          </svg>
+        </ButtonNext>
+      </CarouselProvider>
     </div>
   )
 }
