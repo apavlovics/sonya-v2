03f31b2ec65bcc3565eacbf4792d15ec697dc846
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2021-01-07 21:21:32 +0200

    Improve Gallery layout

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,10 +1,12 @@
 import {useEffect} from 'react'
 import {withRouter} from 'react-router-dom'
-import {CarouselProvider, DotGroup, Image, Slider, Slide, ButtonBack, ButtonNext} from 'pure-react-carousel'
+import {useWindowSize} from '@react-hook/window-size'
+import {CarouselProvider, Dot, DotGroup, Image, Slider, Slide, ButtonBack, ButtonNext} from 'pure-react-carousel'
 import 'pure-react-carousel/dist/react-carousel.es.css'
 import {setScrollingEnabled} from './Utilities'
 
 function Gallery(props) {
+  const [width, height] = useWindowSize()
 
   // Hide the main menu on mount and show on unmount
   useEffect(() => {
@@ -16,15 +18,17 @@ function Gallery(props) {
     }
   })
 
+  const carouselClassName = width / height > 1.5 ? 'horizontal' : 'vertical'
   return (
     <CarouselProvider
+        className={carouselClassName}
         naturalSlideWidth={1}
         isIntrinsicHeight
         hasMasterSpinner
         totalSlides={props.preview.galleryLength}>
       <ButtonBack className="back-button">
-        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" >
-          <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
         </svg>
       </ButtonBack>
       <div className="gallery">
@@ -35,7 +39,36 @@ function Gallery(props) {
             </Slide>
           ))}
         </Slider>
-        <DotGroup className="dots" dotNumbers />
+        <DotGroup
+            className="dot-group"
+            renderDots={props => {
+              const {
+                currentSlide,
+                totalSlides,
+                visibleSlides,
+                disableActiveDots,
+                showAsSelectedForCurrentSlideOnly,
+              } = props
+              const dots = []
+              for (let i = 0; i < totalSlides; i += 1) {
+                const multipleSelected = i >= currentSlide && i < (currentSlide + visibleSlides)
+                const singleSelected = i === currentSlide
+                const selected = showAsSelectedForCurrentSlideOnly ? singleSelected : multipleSelected
+                const slide = i >= totalSlides - visibleSlides ? totalSlides - visibleSlides : i
+                dots.push(
+                  <Dot
+                    key={i}
+                    slide={slide}
+                    selected={selected}
+                    disabled={disableActiveDots ? selected : false}>
+                    <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+                    </svg>
+                  </Dot>,
+                )
+              }
+              return dots
+            }} />
         <button
             id="close-button"
             onClick={() => {
@@ -43,14 +76,13 @@ function Gallery(props) {
               else props.history.push(props.parentPath)
             }}>
           <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
           </svg>
         </button>
       </div>
       <ButtonNext className="next-button">
         <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-          <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+          <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
         </svg>
       </ButtonNext>
     </CarouselProvider>
