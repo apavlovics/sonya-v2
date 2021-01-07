495d2204dedbc3d4df770e42b3a66c919605ffbf
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2021-01-07 20:10:11 +0200

    Improve Gallery layout

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,6 +1,6 @@
 import {useEffect} from 'react'
 import {withRouter} from 'react-router-dom'
-import {CarouselProvider, Image, Slider, Slide, ButtonBack, ButtonNext} from 'pure-react-carousel'
+import {CarouselProvider, DotGroup, Image, Slider, Slide, ButtonBack, ButtonNext} from 'pure-react-carousel'
 import 'pure-react-carousel/dist/react-carousel.es.css'
 import {setScrollingEnabled} from './Utilities'
 
@@ -17,42 +17,43 @@ function Gallery(props) {
   })
 
   return (
-    <div className="gallery-wrapper">
-      <button
-        id="close-button"
-        onClick={() => {
-          if (props.history.length > 1) props.history.goBack()
-          else props.history.push(props.parentPath)
-        }}>
-        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+    <CarouselProvider
+        naturalSlideWidth={1}
+        isIntrinsicHeight
+        hasMasterSpinner
+        totalSlides={props.preview.galleryLength}>
+      <ButtonBack className="back-button">
+        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" >
+          <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
         </svg>
-      </button>
-      <CarouselProvider
-          naturalSlideWidth={1}
-          isIntrinsicHeight
-          hasMasterSpinner
-          totalSlides={props.preview.galleryLength}>
-        <ButtonBack className="back-button">
-          <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" >
-            <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-          </svg>
-        </ButtonBack>
-        <Slider className="gallery">
+      </ButtonBack>
+      <div className="gallery">
+        <Slider className="slider">
           {[...Array(props.preview.galleryLength).keys()].map(key => (
             <Slide key={key} index={key} className="slide">
               <Image src={`/projects/${props.preview.url}/gallery/00${key + 1}.jpg`} />
             </Slide>
           ))}
         </Slider>
-        <ButtonNext className="next-button">
+        <DotGroup className="dots" dotNumbers />
+        <button
+            id="close-button"
+            onClick={() => {
+              if (props.history.length > 1) props.history.goBack()
+              else props.history.push(props.parentPath)
+            }}>
           <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
           </svg>
-        </ButtonNext>
-      </CarouselProvider>
-    </div>
+        </button>
+      </div>
+      <ButtonNext className="next-button">
+        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+        </svg>
+      </ButtonNext>
+    </CarouselProvider>
   )
 }
 
