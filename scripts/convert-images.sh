9e382828e96b2064306744c70e660f5c687a0317
Andrejs Pavlovics <andrejs.pavlovics@gmail.com>
2021-08-29 22:50:25 +0300

    Update script

--- scripts/convert-images.sh
+++ scripts/convert-images.sh
@@ -4,5 +4,5 @@ mkdir -p scripts/output/
 
 # Use ImageMagick to convert JPEG images: https://imagemagick.org
 # On macOS it can be installed via Homebrew: brew install imagemagick
-magick convert 'scripts/input/*.jpg[1920x3840]' -quality 80 -set filename:basename "%[basename]" "scripts/output/%[filename:basename].jpg"
+magick convert 'scripts/input/*.jpg[1920x3840]' -quality 80 -set filename:basename "%[basename]" "scripts/output/%[filename:basename]-v1.jpg"
 echo "Done: JPEG images converted and located at \"scripts/output/\""
