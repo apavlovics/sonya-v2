744119f17d925929d7fd770ebe945eeb1992e4eb
Andrejs Pavlovics <andrejs.pavlovics@gmail.com>
2023-06-16 14:23:27 +0200

    Fix exceptions during tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,7 @@
 // Add custom Jest matchers for asserting on DOM nodes
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+
+// Stub window scrolling during testing
+const noop = () => {}
+Object.defineProperty(window, 'scrollTo', {value: noop, writable: true})
