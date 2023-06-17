7f9f63047b93d240c06c135cff860a7f544e8cb6
Andrejs Pavlovics <andrejs.pavlovics@gmail.com>
2023-06-17 12:07:23 +0200

    Extend unit tests

--- src/Utilities.test.tsx
+++ src/Utilities.test.tsx
@@ -1,8 +1,8 @@
-import {stripSlashes} from './Utilities'
+import {stripPrefix, stripSlashes} from './Utilities'
 
 describe('Utilities should', () => {
 
-  describe('given stripSlashes method', () => {
+  describe('given stripSlashes function', () => {
 
     test('strip slashes from the beginning and the end of the path', () => {
       const paths = ['path', '/path/', '/path', 'path/']
@@ -13,7 +13,19 @@ describe('Utilities should', () => {
     })
 
     test('do not stript slashes from the middle of the path', () => {
-      expect(stripSlashes("/some/other/path/")).toBe("some/other/path")
+      expect(stripSlashes('/some/other/path/')).toBe('some/other/path')
+    })
+  })
+
+  describe('given stripPrefix function', () => {
+
+    test('strip prefix from the path when it is present', () => {
+      expect(stripPrefix('some/test/path', 'some/')).toBe('test/path')
+    })
+
+    test('do not change the path when the prefix is not present', () => {
+      const path = 'some/test/path'
+      expect(stripPrefix(path, 'prefix')).toBe(path)
     })
   })
 })
