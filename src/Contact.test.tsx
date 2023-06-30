5307fc9da6ef8d89f10956babdeaabab2fe21f76
Andrejs Pavlovics <andrejs.pavlovics@gmail.com>
2023-06-30 09:12:54 +0300

    Add Contact test

---
+++ src/Contact.test.tsx
@@ -0,0 +1,16 @@
+import {render, screen} from '@testing-library/react'
+import {HelmetProvider} from 'react-helmet-async'
+import Contact from './Contact'
+
+// Load i18next instance for translations to work
+import './i18n'
+
+describe('Contact should', () => {
+
+  test('render the email', () => {
+    render(<HelmetProvider><Contact /></HelmetProvider>)
+
+    const elements = screen.getAllByText(/sonya@sonya.lv/)
+    expect(elements).toHaveLength(2)
+  })
+})
